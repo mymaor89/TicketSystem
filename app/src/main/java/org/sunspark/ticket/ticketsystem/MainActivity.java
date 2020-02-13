7864ef458f9153273c2446ee7593f6c731e90390
Ilay <ilay778899@gmail.com>
2020-02-13 20:52:40 +0200

    Json file added

--- app/src/main/java/org/sunspark/ticket/ticketsystem/MainActivity.java
+++ app/src/main/java/org/sunspark/ticket/ticketsystem/MainActivity.java
@@ -3,6 +3,9 @@ package org.sunspark.ticket.ticketsystem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
