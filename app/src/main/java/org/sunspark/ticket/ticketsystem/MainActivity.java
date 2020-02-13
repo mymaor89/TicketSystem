14e9da83c06a5eede6154612099efd9b9909b688
Maor Yatskan <mymaor89@gmail.com>
2020-02-13 18:37:09 +0200

    initial commit

---
+++ app/src/main/java/org/sunspark/ticket/ticketsystem/MainActivity.java
@@ -0,0 +1,13 @@
+package org.sunspark.ticket.ticketsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
