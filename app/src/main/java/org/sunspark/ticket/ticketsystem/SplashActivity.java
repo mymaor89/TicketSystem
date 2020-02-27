5506415bc2a51fbe9c118f89ef65cd6fa98afd28
Maor Yatskan <mymaor89@gmail.com>
2020-02-27 15:07:57 +0200

    Fixed gradle issue, countdown timer of splash  screen, few bugs related to login screen.

--- app/src/main/java/org/sunspark/ticket/ticketsystem/SplashActivity.java
+++ app/src/main/java/org/sunspark/ticket/ticketsystem/SplashActivity.java
@@ -1,5 +1,7 @@
 package org.sunspark.ticket.ticketsystem;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new CountDownTimer(2000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+               Intent i = new Intent(SplashActivity.this,MainActivity.class);
+               startActivity(i);
+            }
+        }.start();
+
     }
 }
